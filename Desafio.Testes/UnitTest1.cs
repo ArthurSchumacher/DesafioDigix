3a96b1ae17413b38c8dfe4f83bf82578010b49f6
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2023-02-06 20:50:35 -0400

    Testes automatizados

---
+++ Desafio.Testes/UnitTest1.cs
@@ -0,0 +1,34 @@
+namespace Desafio.Consumo.Tests
+{
+    [TestFixture]
+    public class FamilyOpTests
+    {
+        private FamilyOp _familyOp;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _familyOp = new FamilyOp();
+        }
+
+        [Test]
+        public void Add10_ShouldAdd10FamiliesWithCorrectRandomValues()
+        {
+            // Act
+            _familyOp.Add10();
+
+            // Assert
+            var families = FamilyService.Browse().ToList();
+            Assert.AreEqual(11, families.Count());
+
+            foreach (var family in families)
+            {
+                // Check if the total income is within the expected range
+                Assert.That(family.TotalIncome, Is.InRange(0, 3000));
+
+                // Check if each person's age is within the expected range
+                Assert.That(family.Members.All(m => m.Age >= 0 && m.Age <= 37), Is.True);
+            }
+        }
+    }
+}
---
+++ Desafio.Testes/Usings.cs
@@ -0,0 +1,4 @@
+global using NUnit.Framework;
+global using Desafio.Domain;
+global using Desafio.Service;
+global using System.Linq;
